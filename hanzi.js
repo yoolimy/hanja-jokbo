c85f88cc0480e95616d7b8e89925685369b201c5
yoolimy <nara5502@naver.com>
2026-05-18 13:07:10 +0900

    문제업데이트

---
+++ hanzi.js
@@ -0,0 +1,40 @@
+var hanzilookup = (function() {
+    var _mobiDict = null;
+    var _isInitialized = false;
+
+    function init(type, file, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", file, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 0)) {
+                try {
+                    _mobiDict = JSON.parse(xhr.responseText);
+                    _isInitialized = true;
+                    if (callback) callback();
+                } catch (e) { console.error("JSON 로드 실패"); }
+            }
+        };
+        xhr.send();
+    }
+
+    function recognize(strokes, limit, callback) {
+        if (!_isInitialized || !_mobiDict) return;
+        var userCount = strokes.length;
+        var results = [];
+
+        // 💡 오차 범위 수정: 0(정확히 일치)에서 +1까지만 탐색
+        for (var i = 0; i <= 1; i++) {
+            var count = userCount + i;
+            if (_mobiDict[count]) {
+                _mobiDict[count].forEach(function(char) {
+                    if (!results.some(r => r.character === char)) {
+                        results.push({ character: char });
+                    }
+                });
+            }
+        }
+        callback(results.slice(0, limit));
+    }
+
+    return { init: init, recognize: recognize };
+})();
